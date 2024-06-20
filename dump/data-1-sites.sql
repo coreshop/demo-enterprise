3ceacec0b2150e199271c9d09bc9441495bffdfa
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2024-06-20 14:28:16 +0200

    add enterprise dev domain to site

--- dump/data-1-sites.sql
+++ dump/data-1-sites.sql
@@ -1,4 +1,4 @@
 
 SET NAMES utf8mb4;
 
-INSERT INTO sites (`id`,`mainDomain`,`domains`,`rootId`,`errorDocument`,`localizedErrorDocuments`,`redirectToMainDomain`,`creationDate`,`modificationDate`) VALUES (1,'demo4-enterprise-b2b.coreshop.org','a:3:{i:0;s:39:\"coreshop4-enterprise-b2b-demo.localhost\";i:1;s:38:\"coreshop-enterprise-b2b-demo.localhost\";i:2;s:42:\"coreshop4-enterprise-b2b-demo.ofcors.cloud\";}',41,'','a:3:{s:2:\"en\";s:0:\"\";s:2:\"de\";s:0:\"\";s:2:\"fr\";s:0:\"\";}',0,1703692321,1703693984);
+INSERT INTO sites (`id`,`mainDomain`,`domains`,`rootId`,`errorDocument`,`localizedErrorDocuments`,`redirectToMainDomain`,`creationDate`,`modificationDate`) VALUES (1,'enterprise-b2b.coreshop.dev','a:4:{i:0;s:33:\"demo4-enterprise-b2b.coreshop.org\";i:1;s:39:\"coreshop4-enterprise-b2b-demo.localhost\";i:2;s:38:\"coreshop-enterprise-b2b-demo.localhost\";i:3;s:42:\"coreshop4-enterprise-b2b-demo.ofcors.cloud\";}',41,'','a:3:{s:2:\"en\";s:0:\"\";s:2:\"de\";s:0:\"\";s:2:\"fr\";s:0:\"\";}',0,1703692321,1718886463);

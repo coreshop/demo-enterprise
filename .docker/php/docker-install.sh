727060106015c634a42a9532ead2c7bda9bb05d5
Dominik Pfaffenbauer <pfaffenbauer@cors.gmbh>
2026-09-05 15:06:43 +0200

    Remove committed secrets: Packagist token, JWT keypair, app secret
    COMPOSER_AUTH comes from the environment, the JWT keypair is generated at
    container start (lexik:jwt:generate-keypair), the Symfony secret is read from
    APP_SECRET. Preparation for publishing the repository.

--- .docker/php/docker-install.sh
+++ .docker/php/docker-install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+echo "Generate JWT keypair (headless API)"
+bin/console lexik:jwt:generate-keypair --skip-if-exists --no-interaction || true
+
 echo "Install Pimcore"
 vendor/bin/pimcore-install --skip-database-config --no-interaction
 

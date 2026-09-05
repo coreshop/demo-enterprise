5388cbefbc11586f5402dfbf5f8c5ecb8c1fb2e5
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2026-09-05 17:53:50 +0200

    Keep enterprise-b2b.coreshop.dev as the B2B main domain, drop the coreshop.org name

--- dump/data-1-sites.sql
+++ dump/data-1-sites.sql
@@ -1,4 +1,4 @@
 
 SET NAMES utf8mb4;
 
-INSERT INTO sites (`id`,`mainDomain`,`domains`,`rootId`,`errorDocument`,`localizedErrorDocuments`,`redirectToMainDomain`,`creationDate`,`modificationDate`) VALUES (1,'demo-enterprise-b2b.coreshop.org','a:2:{i:0;s:27:\"enterprise-b2b.coreshop.dev\";i:1;s:38:\"coreshop-enterprise-b2b-demo.localhost\";}',41,'','a:3:{s:2:\"en\";s:0:\"\";s:2:\"de\";s:0:\"\";s:2:\"fr\";s:0:\"\";}',0,1703692321,1718886463);
+INSERT INTO sites (`id`,`mainDomain`,`domains`,`rootId`,`errorDocument`,`localizedErrorDocuments`,`redirectToMainDomain`,`creationDate`,`modificationDate`) VALUES (1,'enterprise-b2b.coreshop.dev','a:1:{i:0;s:38:\"coreshop-enterprise-b2b-demo.localhost\";}',41,'','a:3:{s:2:\"en\";s:0:\"\";s:2:\"de\";s:0:\"\";s:2:\"fr\";s:0:\"\";}',0,1703692321,1718886463);
